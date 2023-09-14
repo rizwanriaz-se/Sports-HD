b7c8b54279eb29adfc4d22ffecd4c86f43860f9b
Rizwan Riaz <iamrizwan077@gmail.com>
2023-09-14 04:21:06 +0000

    Changed site name

--- live_sports/src/Components/Navbar.js
+++ live_sports/src/Components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <div className="navbar   border-b-2 border-gray-600">
             <span className="navbar-start font-bold ml-4 text-2xl">
-            LiveSports
+            SportsHD
             </span>
         </div>
     )
